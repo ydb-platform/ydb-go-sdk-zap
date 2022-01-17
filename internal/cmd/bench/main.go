b70d85d5b009581a130efee432bba184611afb7f
asmyasnikov <79263256394@ya.ru>
2022-01-17 19:14:27 +0300

    fix balancer path

--- internal/cmd/bench/main.go
+++ internal/cmd/bench/main.go
@@ -16,7 +16,7 @@ import (
 	"go.uber.org/zap/zapcore"
 
 	"github.com/ydb-platform/ydb-go-sdk/v3"
-	"github.com/ydb-platform/ydb-go-sdk/v3/config/balancer"
+	"github.com/ydb-platform/ydb-go-sdk/v3/balancer"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table/options"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table/types"
